a69b1adc347b32180a48cec22a1e268c791a94be
Puttu-Kag <puttukag123@gmail.com>
2024-07-25 10:03:29 +0530

    Portcommit

--- src/index.js
+++ src/index.js
@@ -15,3 +15,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// document.querySelector('form').addEventListener('submit', function(event) {
+//   event.preventDefault();
+//   alert('Message sent!');
+// });
